c1f98ae47b87e0d21f074ca2b768d37bb84be768
Himanshu <hv308489@gmail.com>
2025-05-02 22:47:58 +0530

    lawyers signup and listed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,46 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Home";
 import Lawyer from "./Lawyer";
 import Chat from "./Chat";
 import Login from "./Login";
-import Signup from "./Signup"
+import Signup from "./Signup";
+import { AuthProvider, useAuth } from "./utils/authContext";
+
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  
+  if (loading) {
+    return <div className="flex justify-center items-center h-screen">Loading...</div>;
+  }
+  
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+  
+  return children;
+};
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/lawyer" element={<Lawyer />} />
-          <Route path="/chat" element={<Chat />} />
-        </Route>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/lawyer" element={<Lawyer />} />
+            <Route path="/chat" element={
+              <ProtectedRoute>
+                <Chat />
+              </ProtectedRoute>
+            } />
+          </Route>
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
